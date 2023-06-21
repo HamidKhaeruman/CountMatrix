b8052d54cce6ee3385138b74553332f72f84c242
Dimas Cahyo Margono <136548518+dimzkuy@users.noreply.github.com>
2023-06-21 09:18:19 +0700

    Update CountMatrixApp.py
    update penataan fungsi matriks

--- CountMatrixApp.py
+++ CountMatrixApp.py
@@ -1,5 +1,16 @@
-
-
+# (Dimas) >> Fungsi untuk menjalankan penataan elemen matriks
+def sort_matrix(matrix, ascending=True):
+    flattened_matrix = [element for row in matrix for element in row]
+    flattened_matrix.sort(reverse=not ascending)
+    sorted_matrix = []
+    index = 0
+    for _ in range(len(matrix)):
+        row = []
+        for _ in range(len(matrix[0])):
+            row.append(flattened_matrix[index])
+            index += 1
+        sorted_matrix.append(row)
+    return sorted_matrix
 # (Hamid) >> Fungsi untuk menjalankan perhitungan sesuai opsi yang dipilih, di dalamnya juga menerapkan perulangan for
 def perform_operation(matrix_A, matrix_B, operation):
     result = []
